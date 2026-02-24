4135e0789a7f6009ac5300368de7c50d915f6873
Desarrollo y Diseño <jvargases12@cesde.net>
2026-02-24 10:21:17 -0500

    Creación de clases de Person y sus herencias Teachers y Students

--- src/main/java/co/edu/cesde/ga/model/Person.java
+++ src/main/java/co/edu/cesde/ga/model/Person.java
@@ -8,4 +8,68 @@ public class Person {
     String firstName;
     String lastName;
     String status;
+
+    public Person(Long userId, String code, String documentNumber, String firstName, String lastName, String status) {
+        this.userId = userId;
+        this.code = code;
+        this.documentNumber = documentNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+
+    }
+
+    public Person() {
+
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
 }
