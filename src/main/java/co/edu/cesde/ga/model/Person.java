b0c8076bf721c0a7bcdadadb8a6f831bff52e637
LeoT360 <jtorre@cesde.net>
2026-02-24 07:49:38 -0500

    Creación de model person

---
+++ src/main/java/co/edu/cesde/ga/model/Person.java
@@ -0,0 +1,11 @@
+package co.edu.cesde.ga.model;
+
+public class Person {
+
+    Long userId;
+    String code;
+    String documentNumber;
+    String firstName;
+    String lastName;
+    String status;
+}
